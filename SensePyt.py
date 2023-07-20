28bbdd9d74d28592083d5883494394d2b08632ee
jaynabran <brandonalexgil@gmail.com>
2023-07-20 18:54:04 -0500

    wifi_mov_notifTeleg

--- SensePyt.py
+++ SensePyt.py
@@ -3,7 +3,14 @@ import time
 import network
 #from umqtt.simple import MQTTClient
 import ujson
-import urequests
+import urequests as requests
+from utelegram import Bot
+
+#Bot telegram
+CHAT_ID = "5321453259" #mio
+#CHAT_ID = "-863734460" #Grupo
+TOKEN = "6367476178:AAFvyXryIcCb5SX193RGGh30t7sPsoxABuE"
+bot = Bot(TOKEN)
 
 #Config de la red
 SSID = "ALEXIS 2.4_5GETB"
@@ -24,18 +31,26 @@ def wifiConnect():
     print("Conectado a:", SSID)
     print("Info de red:", wifiSensePy.ifconfig())
     
-#LA PARTE DE TELEGRAM
     
+#Notificacion a Telegram
+def notif_Telegram(message):
+    url = "https://api.telegram.org/bot" + TOKEN + "/sendMessage"
+    data = {"chat_id": CHAT_ID, "text": message}
+    response = requests.post(url, json=data)
+    return response.json()
+
 #Sensor_1 de movimiento
 def mov1_sensor(pin):
     if pin.value() == 1:
         print("Se ha detectado un movimiento.")
         machine.Pin(LED1_PIN, machine.Pin.OUT).on()
+        notif_Telegram("ALV, ya valimos!!!")
         time.sleep(10)
         #notificacion
     else:
         print("Todo chevere!!!")
         machine.Pin(LED1_PIN, machine.Pin.OUT).off()
+        notif_Telegram("Todo Cool!!!")
         time.sleep(5)
         
 #Config pines
@@ -46,8 +61,9 @@ def configPines():
 #Config de start
 def setup():
     wifiConnect()
+    #bot.start_loop()
     configPines()
-    
+        
 setup()
 
 #Bucle
